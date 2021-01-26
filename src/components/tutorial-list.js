625419252c1c4e6c07a7d20a6a9551d7f61b5d04
Megan Sullivan <megan@gatsbyjs.com>
2021-01-25 17:01:44 -0800

    feat: update TutorialList on home page to link to tutorial pages

--- src/components/tutorial-list.js
+++ src/components/tutorial-list.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React from 'react'
+import { Link } from 'gatsby'
 
 const TutorialList = ({ posts }) => {
   return (
     <ul>
       {posts.map((post) => (
         <li>
-          <h2>{post.title}</h2>
-          <p>{post.description}</p>
+          <h2><Link to={post.slug}>{post.frontmatter.title}</Link></h2>
+          <p>{post.frontmatter.description}</p>
         </li>
       ))}
     </ul>

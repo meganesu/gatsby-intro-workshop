f7fcd5d38cf1c2395511327f188aca222fd2faf7
Megan Sullivan <megan@gatsbyjs.com>
2021-01-27 13:02:19 -0800

    feat: create CalloutBox component (to use instead of abusing Markdown blockquotes)

---
+++ src/components/callout-box.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CalloutBox = ({children}) => (
+	<aside style={{
+		backgroundColor: 'lightblue',
+		padding: '0.5rem 1rem',
+	}}>
+		{children}
+	</aside>
+)
+
+export default CalloutBox

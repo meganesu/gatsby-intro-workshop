baf6e43b61863c16ae650063f48bae2e9c3e406c
Megan Sullivan <megan@gatsbyjs.com>
2021-02-22 18:20:20 -0800

    fix: move footer logo out of main element

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,23 +6,27 @@ const pageStyles = {
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
   lineHeight: "1.5rem",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: "2rem",
   lineHeight: "1.25em",
-}
+};
 
-const Layout = ({children, pageContext}) => (
-  <main style={pageStyles}>
-    <title>{pageContext.frontmatter.title}</title>
-		<h1 style={headingStyles}>{pageContext.frontmatter.title}</h1>
-    {children}
-    <img
-      alt="Gatsby G Logo"
-      src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-    />
-  </main>
+const Layout = ({ children, pageContext }) => (
+  <>
+    <main style={pageStyles}>
+      <title>{pageContext.frontmatter.title}</title>
+      <h1 style={headingStyles}>{pageContext.frontmatter.title}</h1>
+      {children}
+    </main>
+    <footer>
+      <img
+        alt="Gatsby G Logo"
+        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+      />
+    </footer>
+  </>
 );
 
 export default Layout;

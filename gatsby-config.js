43af96d78e8eedb237b9b72056fe46bcb47c1e52
Nachiket Kumar <nk@nachiketkumar.com>
2021-02-24 14:35:25 -0500

    add theme ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-remark-images`,
     `gatsby-plugin-twitter`,
+    `gatsby-plugin-theme-ui`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {

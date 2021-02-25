b41a5438a11b6bfb20fd92000fc52a88a59d4c5d
Nachiket Kumar <nk@nachiketkumar.com>
2021-02-25 11:09:16 -0500

    basic footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,6 +31,14 @@ const eyebrowStyles = {
   boxShadow: (theme) => `0 0 0 2px ${theme.colors.purple20}`,
 }
 
+const footerStyles = {
+  display: 'flex',
+  alignItems: 'flex-start',
+  pt: 3,
+  mt: [5,7],
+  borderTop: (theme) => `1px solid ${theme.colors.purple20}`,
+}
+
 const Layout = ({ children, pageContext }) => (
   <div sx={bodyStyles}>
     <main>
@@ -39,11 +47,12 @@ const Layout = ({ children, pageContext }) => (
       <Text variant="h1" as="h1" sx={titleStyles}><span sx={eyebrowStyles}>{pageContext.frontmatter.eyebrow}</span>{pageContext.frontmatter.title}</Text>
       {children}
     </main>
-    <footer>
+    <footer sx={footerStyles}>
       <img
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
+      <Text variant="small" sx={{m:'0px', ml: 2}}>&copy; 2021 Gatsby Inc. <a href="https://www.gatsbyjs.com">Go to the GatsbyJS homepage &rarr;</a></Text>
     </footer>
   </div>
 );
